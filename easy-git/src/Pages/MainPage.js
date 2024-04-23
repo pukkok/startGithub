6bde5d38c7f1015c9f6b5a6a728e74fd8c291538
Pukkok <krystalgggg@gmail.com>
2024-04-24 00:11:06 +0900

    깃허브 사용법 익히기

---
+++ easy-git/src/Pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChapterA from './ChapterA'
+
+function MainPage (){
+    return(
+        <>
+            <ChapterA/>
+        </>
+    )
+}
+
+export default MainPage
